9fc34dde34840c4f2b28cacec8c42ae70cb4565b
shreya <shreya.sdeshpande06@gmail.com>
2020-09-14 21:34:29 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 
-function App() {
-  return (
-    <div>
-        Phone Directory
-    </div>
-  );
+class App extends Component{
+    render() {
+        return (
+            <div>
+                {/*<div className="header">*/}
+                {/*    Phone Directory*/}
+                {/*</div>*/}
+                {/*<button> Add </button>*/}
+                {/*<div>*/}
+                {/*    <span> Name </span><br/>*/}
+                {/*    <span> Phone </span>*/}
+                {/*</div>*/}
+                <label htmlFor="name"> Name: </label>
+                <input id="name" type="text" placeholder="Type Here" defaultValue="Shilpa"/>
+            </div>
+        );
+    }
 }
 
 export default App;

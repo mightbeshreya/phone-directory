1c5d1f445b339783199a50d00e9ce0bd53372613
shreya <shreya.sdeshpande06@gmail.com>
2020-09-14 23:08:14 +0530

    JSX and JS

--- src/App.js
+++ src/App.js
@@ -3,19 +3,30 @@ import React, {Component} from 'react';
 
 class App extends Component{
     render() {
+        let x = 5;
+        let y = 9;
         return (
-            <div>
-                {/*<div className="header">*/}
-                {/*    Phone Directory*/}
-                {/*</div>*/}
-                {/*<button> Add </button>*/}
-                {/*<div>*/}
-                {/*    <span> Name </span><br/>*/}
-                {/*    <span> Phone </span>*/}
-                {/*</div>*/}
-                <label htmlFor="name"> Name: </label>
-                <input id="name" type="text" placeholder="Type Here" defaultValue="Shilpa"/>
+
+
+
+               // {/*<div className="header">*/}
+               // {/*    Phone Directory*/}
+               // {/*</div>*/}
+               // {/*<button> Add </button>*/}
+               // {/*<div>*/}
+               // {/*    <span> Name </span><br/>*/}
+               // {/*    <span> Phone </span>*/}
+               // {/*</div>*/}
+               // {/*<label htmlFor="name"> Name: </label>*/}
+               // {/*<input id="name" type="text" placeholder="Type Here" defaultValue={x+y}/>*/}
+
+            <div id="module">
+                <p>ReactJS</p>
             </div>
+
+            /*React.createElement("div", {id:"module"},
+                    React.createElement("p", null, "ReactJS")
+            )*/
         );
     }
 }

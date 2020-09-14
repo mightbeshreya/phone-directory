8715f95f929ebfdacd016ec2625f6c24a1f086f5
shreya <shreya.sdeshpande06@gmail.com>
2020-09-15 00:37:14 +0530

    Header as common

--- src/App.js
+++ src/App.js
@@ -1,33 +1,37 @@
 import React, {Component} from 'react';
-
+import Header from './Header';
+//This Header refers to const Header in function, then ./Header means ./Header.js
 
 class App extends Component{
     render() {
-        let x = 5;
-        let y = 9;
+        //let x = 5;
+        //let y = 9;
         return (
-
-
-
-               // {/*<div className="header">*/}
-               // {/*    Phone Directory*/}
-               // {/*</div>*/}
-               // {/*<button> Add </button>*/}
-               // {/*<div>*/}
-               // {/*    <span> Name </span><br/>*/}
-               // {/*    <span> Phone </span>*/}
-               // {/*</div>*/}
-               // {/*<label htmlFor="name"> Name: </label>*/}
-               // {/*<input id="name" type="text" placeholder="Type Here" defaultValue={x+y}/>*/}
-
-            <div id="module">
+            <div>
+                {/*<div className="header">
+                   Phone Directory
+               </div>*/}
+               <Header/>
+               <button> Add </button>
+               <div>
+                   <span> Name </span><br/>
+                   <span> Phone </span>
+               </div>
+                {/*<label htmlFor="name"> Name: </label>
+               <input id="name" type="text" placeholder="Type Here" defaultValue={x+y}/> */}
+            </div>
+            /*<div id="module">
                 <p>ReactJS</p>
             </div>
 
             /*React.createElement("div", {id:"module"},
                     React.createElement("p", null, "ReactJS")
             )*/
+          /*  <h1>Welcome To React</h1>
+            <div>Hi</div> */
         );
+        //return React.createElement("h1", null, "Welcome to React");
+        //return React.createElement("div", null, "Hi");
     }
 }
 

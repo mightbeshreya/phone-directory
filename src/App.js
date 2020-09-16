4e6aff2dbe6a2af8f064e80c6fb26319023bc8f1
shreya <shreya@zyllu.com>
2020-09-16 23:53:51 +0530

    Styled ‘Header’ (Inline Styling)

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import Header from './Header';
 //This Header refers to const Header in function, then ./Header means ./Header.js
 
-class App extends Component{
+/*class App extends Component{
     render() {
         //let x = 5;
         //let y = 9;
@@ -10,16 +10,16 @@ class App extends Component{
             <div>
                 {/*<div className="header">
                    Phone Directory
-               </div>*/}
-               <Header/>
+               </div>*///}
+               /*<Header/>
                <button> Add </button>
                <div>
                    <span> Name </span><br/>
                    <span> Phone </span>
                </div>
                 {/*<label htmlFor="name"> Name: </label>
-               <input id="name" type="text" placeholder="Type Here" defaultValue={x+y}/> */}
-            </div>
+               <input id="name" type="text" placeholder="Type Here" defaultValue={x+y}/> *///}
+            /*</div>
             /*<div id="module">
                 <p>ReactJS</p>
             </div>
@@ -29,9 +29,24 @@ class App extends Component{
             )*/
           /*  <h1>Welcome To React</h1>
             <div>Hi</div> */
-        );
+        //);
         //return React.createElement("h1", null, "Welcome to React");
         //return React.createElement("div", null, "Hi");
+    /*}
+}*/
+
+class App extends Component{
+    render() {
+        return (
+            <div>
+                <Header/>
+                <button> Add </button>
+                <div>
+                    <span> Name </span><br/>
+                    <span> Phone </span>
+                </div>
+            </div>
+        );
     }
 }
 
--- src/Header.js
+++ src/Header.js
@@ -2,12 +2,23 @@
 import React from 'react';
 
 const Header = function() {
+    const headerStyle = {textAlign:'center', padding: 20, background: '#000'
+        , color: '#fff', textTransform:'uppercase' };
     return(
-        <div className="header">
+        <div style={headerStyle}>
             Phone Directory
         </div>
     )
 }
+
+/*const Header = function() {
+    return(
+        <div style={{textAlign:'center', padding: 20, background: '#000'
+            , color: '#fff', textTransform:'uppercase' }}>
+            Phone Directory
+        </div>
+    )
+} */
 
 
 // class Header extends Component {

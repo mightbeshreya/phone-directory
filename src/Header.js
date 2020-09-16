23e41d0b42409fda377cf7ed4f41aec42d9b82d9
shreya <shreya.sdeshpande06@gmail.com>
2020-09-17 00:08:13 +0530

    text-transform: uppercase;

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,16 @@
 //import React, {Component} from 'react';
 import React from 'react';
+import './Header.css';
 
 const Header = function() {
+    return(
+        <div className='header'>
+            Phone Directory
+        </div>
+    )
+}
+
+/*const Header = function() {
     const headerStyle = {textAlign:'center', padding: 20, background: '#000'
         , color: '#fff', textTransform:'uppercase' };
     return(
@@ -9,7 +18,7 @@ const Header = function() {
             Phone Directory
         </div>
     )
-}
+}*/
 
 /*const Header = function() {
     return(
